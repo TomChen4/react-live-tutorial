1491da627c2eeb2aa7d336853fc8b4c57c549521
Tom Chen <tomchen4@gmail.com>
2017-06-13 23:31:42 +0300

    add data

---
+++ src/todos.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tom_c on 13/06/2017.
+ */
+export default {
+    todos:[
+        {id:1, text:'First Todo'},
+        {id:2, text:'Second Todo'},
+        {id:3, text:'Third Todo'},
+        {id:4, text:'Fourth Todo'}
+    ],
+    id:4
+}
